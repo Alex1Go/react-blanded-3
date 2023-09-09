431ad04298d8010ea1a94fe78e5176feddbc6e5f
Rummut <kzantip@gmail.com>
2023-09-09 12:49:15 +0300

    add CountryInfo

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,15 @@
 import { Grid, GridItem } from 'components';
+import { Link } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
   return (
     <Grid>
       {countries.map(({ id, flag, country }) => (
         <GridItem key={id}>
-          <img src={flag} alt={country} />
-          <p>{country}</p>
+          <Link to={`/country/${id}`}>
+            <img src={flag} alt={country} />
+            <p>{country}</p>
+          </Link>
         </GridItem>
       ))}
     </Grid>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,11 +1,27 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [countries, setCountries] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchCountry(countryId)
+      .then(data => setCountries(data))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [countryId]);
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo {...countries} />
       </Container>
+      {loading && <Loader />}
     </Section>
   );
 };
